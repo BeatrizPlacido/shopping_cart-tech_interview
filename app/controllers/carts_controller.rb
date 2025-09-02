76e3c5d52a40bcc71a581d67f77d1fa941f3f1a0
BeatrizPlacido <biaplacsilva0212@gmail.com>
2025-09-02 01:06:09 -0300

    feat: Implement cart management with index, show, and create actions in CartsController

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,29 @@
 class CartsController < ApplicationController
-  ## TODO Escreva a lógica dos carrinhos aqui
+  before_action :set_cart, only: %i[ show ]
+
+  def index
+    @carts = Cart.all
+
+    render json: @carts
+  end
+
+  def show
+    render json: @cart
+  end
+
+  def create
+    @cart = CartService.new.create_cart
+
+    if @cart.save
+      render json: @cart, status: :created, location: @cart
+    else
+      render json: @cart.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_cart
+    @cart = Cart.find(params[:id])
+  end
 end
