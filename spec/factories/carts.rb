8950e57fc9c3b1ad0b15c5aa068dbdae1a7fcb69
BeatrizPlacido <biaplacsilva0212@gmail.com>
2025-11-01 19:51:06 -0300

    feat: Enhance cart functionality with inactivity tracking and abandonment logic

--- spec/factories/carts.rb
+++ spec/factories/carts.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :cart do
     total_price { 0.0 }
     status { :open }
+    last_interaction_at { Time.current }
 
     trait :abandoned do
       status { :abandoned }
