5f80e5963d3a4a9454518cd476dfc42954dcaee5
BeatrizPlacido <biaplacsilva0212@gmail.com>
2025-11-01 20:16:50 -0300

    refactor: Simplify cart abandonment logic and update specs for last interaction tracking

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -1,31 +1,26 @@
 class MarkCartAsAbandonedJob
   include Sidekiq::Job
 
-  INACTIVITY_THRESHOLD = 3.hours
-  EXPIRED_THRESHOLD = 7.days
-
   def perform
     mark_inactive_carts_as_abandoned
-    mark_as_expired_old_abandoned_carts
+    remove_abandoned_carts
   end
 
   private
 
   def mark_inactive_carts_as_abandoned
-    abandoned_candidate_carts = Cart.where(status: 'open')
-                                    .where('updated_at < ?', INACTIVITY_THRESHOLD.ago)
+    abandoned_candidate_carts = Cart.in_inactive_threshold.where(status: 'open')
 
     return if abandoned_candidate_carts.blank?
 
-    abandoned_candidate_carts.update_all(status: 'abandoned', updated_at: Time.current)
+    abandoned_candidate_carts.each { |cart| cart.mark_as_abandoned! }
   end
 
-  def mark_as_expired_old_abandoned_carts
+  def remove_abandoned_carts
     expired_candidate_carts = Cart.where(status: 'abandoned')
-                                  .where('updated_at < ?', EXPIRED_THRESHOLD.ago)
 
     return if expired_candidate_carts.blank?
 
-    expired_candidate_carts.update_all(status: 'expired', updated_at: Time.current)
+    expired_candidate_carts.each{ |cart| cart.remove_if_abandoned }
   end
 end
